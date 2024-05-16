e1feb5d7e62f29a3f2a143436a585351d9e45881
Jean Carlos <jean522010@hotmail.com>
2024-05-15 23:00:55 -0300

    First-Project-Java

--- Projeto-Java/src/App.java
+++ Projeto-Java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Iniciando o Projeto Java");
+        System.out.println("Iniciando o Projeto Java!");
     }
 }
